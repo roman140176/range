20d13c8ee44ee0753706619bc338736d64a76a11
Гордеев Роман <gordeev.roman@infourok.biz>
2021-12-14 08:09:58 +0300

    Запиздячил карочм компоненту бля

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,21 @@
 import React, {useState} from 'react';
 import Range from "./components/Range/Range";
 import './style.scss'
-import {log} from "util";
 function App() {
-const min = 100000;
-    const max = 600000;
-    const step = 1000;
-    const currency = 'руб.';
-    const [value,setValue] = useState<number>(100000)
-    const [amountValue,setAmountValue] = useState<string>("0")
-    const [width,setWidth] = useState<string>("0")
-    function changeInput(e: React.ChangeEvent<HTMLInputElement>) {
-        const newValue = Math.round(+e.target.value);
-        setValue(newValue)
-        setAmountValue(newValue.toString())
-        let pr = ((newValue - min) * 100) / (max - min);
-        console.log('CHANGE', pr)
-        setWidth((pr).toString() + '%')
-
-    }
-
-    function amountInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-        setAmountValue(e.target.value.replace(/[^\d-]/g, ""))
-        setValue(Math.round(+e.target.value))
-        let pr = ((Math.round(+e.target.value) - min)* 100) / (max - min);
-        setWidth((pr).toString() + '%');
-        if(Math.round(+e.target.value) < min){
-            setWidth( '0%');
-        }
-        if(Math.round(+e.target.value) > max){
-            setWidth( '100%');
-        }
-    }
-
-    function amountBlur(e: React.ChangeEvent<HTMLInputElement>) {
-        if(parseInt(amountValue) > max){
-            setAmountValue(max.toString())
-        }
-        if(parseInt(amountValue) < min){
-            setAmountValue(min.toString())
-        }
-
-        if(amountValue == ''){
-            setAmountValue(min.toString())
-        }
-
-    }
-
-
-    function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
-        if(!amountValue[0] || amountValue[0]==="0"){
-            setAmountValue('')
-        }
-
-    }
 
     return (
     <div className="App">
         <div className="container">
-             <Range value={value}
-             min={min}
-             max={max}
-             step={step}
-             changeInput={(e)=>changeInput(e)}
-             currency={currency}
-             amountValue={amountValue}
-             amountMin={min}
-             amountMax={max}
-             amountInputChange = {(e)=>amountInputChange(e)}
+             <Range
+             min={100000}
+             max={600000}
+             step={1000}
+             currency={'руб.'}
              maxLength = {7}
-             amountBlur = {(e)=>amountBlur(e)}
-             handleKeyPress = {(e)=>handleKeyPress(e)}
              label = {'Стоимость репетитора'}
              showMinMax = {false}
              preliminary={'Предварительный расчёт'}
-             width={width}
+             stateValue={100000}
              />
         </div>
     </div>
--- src/components/Range/Range.tsx
+++ src/components/Range/Range.tsx
@@ -1,83 +1,115 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import styles from "./Range.module.scss";
 
 interface RangeProps {
-    value:number;
     min:number;
     max:number;
     step:number;
-    changeInput:(e:React.ChangeEvent<HTMLInputElement>)=>void;
-    amountInputChange:(e:React.ChangeEvent<HTMLInputElement>)=>void;
-    amountBlur:(e:React.ChangeEvent<HTMLInputElement>)=>void;
-    handleKeyPress:(e:React.KeyboardEvent<HTMLInputElement>)=>void;
     currency:string;
-    amountValue:string;
-    amountMin:number;
-    amountMax:number;
     maxLength:number
     label?:string;
     showMinMax:boolean;
     preliminary:string;
-    width:string;
+    stateValue:number;
 }
 export const Range:FC<RangeProps> = ({
-value,
 min,
 max,
 step,
-changeInput,
 currency,
-amountValue,
-amountMin,
-amountMax,
-amountInputChange,
 maxLength,
-amountBlur,
-handleKeyPress,
 label,
 showMinMax,
 preliminary,
-width,
+stateValue,
 ...props
 }):JSX.Element => {
 
+    const [range,setValue] = useState<number>(stateValue)
+    const [amount,setAmountValue] = useState<string>("0")
+    const [progress,setWidth] = useState<string>("0")
+
+    function changeInput(e: React.ChangeEvent<HTMLInputElement>) {
+        const newValue = Math.round(+e.target.value);
+        setValue(newValue)
+        setAmountValue(newValue.toString())
+        let pr = ((newValue - min) * 100) / (max - min);
+        setWidth((pr).toString() + '%')
+
+    }
+
+    function amountInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setAmountValue(e.target.value.replace(/[^\d-]/g, ""))
+        setValue(Math.round(+e.target.value))
+        let pr = ((Math.round(+e.target.value) - min)* 100) / (max - min);
+        setWidth((pr).toString() + '%');
+        if(Math.round(+e.target.value) < min){
+            setWidth( '0%');
+        }
+        if(Math.round(+e.target.value) > max){
+            setWidth( '100%');
+        }
+    }
+
+    function amountBlur(e: React.ChangeEvent<HTMLInputElement>) {
+        if(parseInt(amount) > max){
+            setAmountValue(max.toString())
+        }
+        if(parseInt(amount) < min){
+            setAmountValue(min.toString())
+        }
+
+        if(amount == ''){
+            setAmountValue(min.toString())
+        }
+
+    }
+
+
+    function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+        if(!amount[0] || amount[0]==="0"){
+            setAmountValue('')
+        }
+
+    }
+
     return (
         <div className={styles.rangeBox}>
-            <div className={styles.rangeBox__selected}>
-                <div className={styles.rangeBox__label}>{label}</div>
+            <div className={styles.selected}>
+                <div className={styles.rangeLabel}>{label}</div>
                <input
                 type="text"
                 inputMode='numeric'
                 pattern='[0-9]*'
                 onKeyPress={handleKeyPress}
                 maxLength={maxLength}
-                value={amountValue}
-                data-min={amountMin}
-                data-max={amountMax}
-                className={styles.rangeBox__amount}
+                value={amount}
+                data-min={min}
+                data-max={max}
+                className={styles.amount}
                 onChange={amountInputChange}
                 onBlur={amountBlur}
                />
 
-                <div className={styles.rangeBox__sumWrapper}>
-                    <div className={styles.rangeBox__sum}>{amountValue == "" ? "0" : amountValue}</div>
-                    <div className={styles.rangeBox__currency}>&nbsp;{currency}</div>
+                <div className={styles.sumWrapper}>
+                    <div className={styles.sum}>{amount == "" ? "0" : amount}</div>
+                    <div className={styles.currency}>&nbsp;{currency}</div>
                 </div>
             </div>
-            <div className={styles.rangeBox__inputWrap}>
-                <input className={styles.rangeBox__input} type="range" step={step} min={min} max={max} value={value} onChange={changeInput}/>
-                 <div className={styles.rangeBox__progress} style={{'width':width}}></div>
-                 <div className={styles.rangeBox__bg}></div>
+            <div className={styles.inputWrap}>
+                <input className={styles.rangeInput} type="range" step={step} min={min} max={max} value={range} onChange={changeInput}/>
+                 <div className={styles.progress} style={{'width':progress}}></div>
+                 <div className={styles.bg}></div>
             </div>
             {
                 showMinMax ?
-                    <div className={styles.rangeBox__minmax}>
-                        <div className={styles.rangeBox__min}>{min>=1000 ? min / 1000 + ' тыс.' : min}</div>
-                        <div className={styles.rangeBox__max}>{max>=1000 ? max / 1000 + ' тыс.' : max}</div>
+                    <div className={styles.minmax}>
+                        <div className={styles.min}>{min>=1000 ? min / 1000 + ' тыс.' : min}</div>
+                        <div className={styles.max}>{max>=1000 ? max / 1000 + ' тыс.' : max}</div>
                     </div>
                     :
-                    <div className={styles.rangeBox__calculation}>
-                        <div className={styles.rangeBox__preliminary}>{preliminary}</div>
+                    <div className={styles.calculation}>
+                        <div className={styles.preliminary}>{preliminary}</div>
                     </div>
             }
 
